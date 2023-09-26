4cb637b0c6483a163b17fc1e406038d452366c47
Miguel Negron <manegron@redborder.com>
2023-09-26 23:47:35 +0100

    Match metadata version with tag

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'javiercrg@redborder.com, jhernandez@redborder.com'
 license          "AGPL"
 description      'Installs/Configures rsyslog in redborder manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.5'
