c463f4cf8f5ea48e4f5783674fcac3e6e6ce3f25
Miguel Negron <manegron@redborder.com>
2024-05-28 17:58:13 +0100

    Lint recipes

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -1,18 +1,16 @@
-#
-# Cookbook Name:: rsyslog
+# Cookbook:: rsyslog
 # Recipe:: default
-#
-# Copyright 2016, redborder
-#
-# All rights reserved - Do Not Redistribute
-rsyslog_config "add" do
-  enable_tls node["redborder"]["rsyslog"]["enable_tls"]
-  config_dir node["redborder"]["rsyslog"]["config_dir"]
-  certificates_dir node["redborder"]["rsyslog"]["certificates_dir"]
-  rules_dir node["redborder"]["rsyslog"]["rules_dir"]
-  remote_logs_dir node["redborder"]["rsyslog"]["remote_logs_dir"]
-  work_dir node["redborder"]["rsyslog"]["work_dir"]
-  user node["redborder"]["rsyslog"]["user"]
-  group node["redborder"]["rsyslog"]["group"]
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
+
+rsyslog_config 'add' do
+  enable_tls node['redborder']['rsyslog']['enable_tls']
+  config_dir node['redborder']['rsyslog']['config_dir']
+  certificates_dir node['redborder']['rsyslog']['certificates_dir']
+  rules_dir node['redborder']['rsyslog']['rules_dir']
+  remote_logs_dir node['redborder']['rsyslog']['remote_logs_dir']
+  work_dir node['redborder']['rsyslog']['work_dir']
+  user node['redborder']['rsyslog']['user']
+  group node['redborder']['rsyslog']['group']
   action :add
 end
